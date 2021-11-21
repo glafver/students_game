fc9ae68104aa1ae3ab1261cf2f2c848d3491b0c3
glafver <glafver22@gmail.com>
2021-11-21 23:22:42 +0100

    Changed shuffling method from sort to Fisher-Yeyts method

--- app.js
+++ app.js
@@ -18,143 +18,152 @@ const students = [{
         "name": "Benjamin Tsubarah",
         "image": "assets/images/students/benjamin-tsubarah.jpg",
     },
-    // {
-    //     "name": "Calle Nilsson",
-    //     "image": "assets/images/students/calle-nilsson.jpg",
-    // },
-    // {
-    //     "name": "Chikage Takahashi Molander",
-    //     "image": "assets/images/students/chikage-takahashi-molander.jpg",
-    // },
-    // {
-    //     "name": "Daniel Be",
-    //     "image": "assets/images/students/daniel-be.jpg",
-    // },
-    // {
-    //     "name": "Daniel Carlsson",
-    //     "image": "assets/images/students/daniel-carlsson.jpg",
-    // },
-    // {
-    //     "name": "Elin Ahlgren",
-    //     "image": "assets/images/students/elin-ahlgren.jpg",
-    // },
-    // {
-    //     "name": "Emma Käck",
-    //     "image": "assets/images/students/emma-kack.jpg",
-    // },
-    // {
-    //     "name": "Eric Ståhl",
-    //     "image": "assets/images/students/eric-stahl.jpg",
-    // },
-    // {
-    //     "name": "Frans Gustavson Påsse",
-    //     "image": "assets/images/students/frans-gustavson-passe.jpg",
-    // },
-    // {
-    //     "name": "Glafira Veretennikova",
-    //     "image": "assets/images/students/glafira-veretennikova.jpg",
-    // },
-    // {
-    //     "name": "Gustaf Grönlund",
-    //     "image": "assets/images/students/gustaf-gronlund.jpg",
-    // },
-    // {
-    //     "name": "Hanna Håkanson",
-    //     "image": "assets/images/students/hanna-hakanson.jpg",
-    // },
-    // {
-    //     "name": "Heidi Sjöberg",
-    //     "image": "assets/images/students/heidi-sjoberg.jpg",
-    // },
-    // {
-    //     "name": "Hugo Carzborn",
-    //     "image": "assets/images/students/hugo-carzborn.jpg",
-    // },
-    // {
-    //     "name": "Jesper Kling",
-    //     "image": "assets/images/students/jesper-kling.jpg",
-    // },
-    // {
-    //     "name": "Johan Ranestam",
-    //     "image": "assets/images/students/johan-ranestam.jpg",
-    // },
-    // {
-    //     "name": "Johanna Bäckström",
-    //     "image": "assets/images/students/johanna-backstrom.jpg",
-    // },
-    // {
-    //     "name": "Johanna Jönsson",
-    //     "image": "assets/images/students/johanna-jonsson.jpg",
-    // },
-    // {
-    //     "name": "Jona Torsson",
-    //     "image": "assets/images/students/jona-torsson.jpg",
-    // },
-    // {
-    //     "name": "Josefine Ahlstedt",
-    //     "image": "assets/images/students/josefine-ahlstedt.jpg",
-    // },
-    // {
-    //     "name": "Julia Jespersdotter Högman",
-    //     "image": "assets/images/students/julia-jespersdotter-hogman.jpg",
-    // },
-    // {
-    //     "name": "Julia Nemell",
-    //     "image": "assets/images/students/julia-nemell.jpg",
-    // },
-    // {
-    //     "name": "Linus Lindberg",
-    //     "image": "assets/images/students/linus-lindberg.jpg",
-    // },
-    // {
-    //     "name": "Malin Olsson",
-    //     "image": "assets/images/students/malin-olsson.jpg",
-    // },
-    // {
-    //     "name": "Maria Haara-Lundhammar",
-    //     "image": "assets/images/students/maria-haara-lundhammar.jpg",
-    // },
-    // {
-    //     "name": "Maria Lövgren",
-    //     "image": "assets/images/students/maria-lovgren.jpg",
-    // },
-    // {
-    //     "name": "Nikola Dimitrijoski",
-    //     "image": "assets/images/students/nikola-dimitrijoski.jpg",
-    // },
-    // {
-    //     "name": "Paulina Kiendys",
-    //     "image": "assets/images/students/paulina-kiendys.jpg",
-    // },
-    // {
-    //     "name": "Raymond Lam",
-    //     "image": "assets/images/students/raymond-lam.jpg",
-    // },
-    // {
-    //     "name": "Robin Karlsson",
-    //     "image": "assets/images/students/robin-karlsson.jpg",
-    // },
-    // {
-    //     "name": "Sara Almqvist",
-    //     "image": "assets/images/students/sara-almqvist.jpg",
-    // },
-    // {
-    //     "name": "Tim Nilsson",
-    //     "image": "assets/images/students/tim-nilsson.jpg",
-    // },
-    // {
-    //     "name": "Tirapat Sukjit",
-    //     "image": "assets/images/students/tirapat-sukjit.jpg",
-    // },
-    // {
-    //     "name": "Tobias Silfverberg",
-    //     "image": "assets/images/students/tobias-silfverberg.jpg",
-    // },
-    // {
-    //     "name": "Wiktoria Dobrzewinska",
-    //     "image": "assets/images/students/wiktoria-dobrzewinska.jpg",
-    // },
+    {
+        "name": "Calle Nilsson",
+        "image": "assets/images/students/calle-nilsson.jpg",
+    },
+    {
+        "name": "Chikage Takahashi Molander",
+        "image": "assets/images/students/chikage-takahashi-molander.jpg",
+    },
+    {
+        "name": "Daniel Be",
+        "image": "assets/images/students/daniel-be.jpg",
+    },
+    {
+        "name": "Daniel Carlsson",
+        "image": "assets/images/students/daniel-carlsson.jpg",
+    },
+    {
+        "name": "Elin Ahlgren",
+        "image": "assets/images/students/elin-ahlgren.jpg",
+    },
+    {
+        "name": "Emma Käck",
+        "image": "assets/images/students/emma-kack.jpg",
+    },
+    {
+        "name": "Eric Ståhl",
+        "image": "assets/images/students/eric-stahl.jpg",
+    },
+    {
+        "name": "Frans Gustavson Påsse",
+        "image": "assets/images/students/frans-gustavson-passe.jpg",
+    },
+    {
+        "name": "Glafira Veretennikova",
+        "image": "assets/images/students/glafira-veretennikova.jpg",
+    },
+    {
+        "name": "Gustaf Grönlund",
+        "image": "assets/images/students/gustaf-gronlund.jpg",
+    },
+    {
+        "name": "Hanna Håkanson",
+        "image": "assets/images/students/hanna-hakanson.jpg",
+    },
+    {
+        "name": "Heidi Sjöberg",
+        "image": "assets/images/students/heidi-sjoberg.jpg",
+    },
+    {
+        "name": "Hugo Carzborn",
+        "image": "assets/images/students/hugo-carzborn.jpg",
+    },
+    {
+        "name": "Jesper Kling",
+        "image": "assets/images/students/jesper-kling.jpg",
+    },
+    {
+        "name": "Johan Ranestam",
+        "image": "assets/images/students/johan-ranestam.jpg",
+    },
+    {
+        "name": "Johanna Bäckström",
+        "image": "assets/images/students/johanna-backstrom.jpg",
+    },
+    {
+        "name": "Johanna Jönsson",
+        "image": "assets/images/students/johanna-jonsson.jpg",
+    },
+    {
+        "name": "Jona Torsson",
+        "image": "assets/images/students/jona-torsson.jpg",
+    },
+    {
+        "name": "Josefine Ahlstedt",
+        "image": "assets/images/students/josefine-ahlstedt.jpg",
+    },
+    {
+        "name": "Julia Jespersdotter Högman",
+        "image": "assets/images/students/julia-jespersdotter-hogman.jpg",
+    },
+    {
+        "name": "Julia Nemell",
+        "image": "assets/images/students/julia-nemell.jpg",
+    },
+    {
+        "name": "Linus Lindberg",
+        "image": "assets/images/students/linus-lindberg.jpg",
+    },
+    {
+        "name": "Malin Olsson",
+        "image": "assets/images/students/malin-olsson.jpg",
+    },
+    {
+        "name": "Maria Haara-Lundhammar",
+        "image": "assets/images/students/maria-haara-lundhammar.jpg",
+    },
+    {
+        "name": "Maria Lövgren",
+        "image": "assets/images/students/maria-lovgren.jpg",
+    },
+    {
+        "name": "Nikola Dimitrijoski",
+        "image": "assets/images/students/nikola-dimitrijoski.jpg",
+    },
+    {
+        "name": "Paulina Kiendys",
+        "image": "assets/images/students/paulina-kiendys.jpg",
+    },
+    {
+        "name": "Raymond Lam",
+        "image": "assets/images/students/raymond-lam.jpg",
+    },
+    {
+        "name": "Robin Karlsson",
+        "image": "assets/images/students/robin-karlsson.jpg",
+    },
+    {
+        "name": "Sara Almqvist",
+        "image": "assets/images/students/sara-almqvist.jpg",
+    },
+    {
+        "name": "Tim Nilsson",
+        "image": "assets/images/students/tim-nilsson.jpg",
+    },
+    {
+        "name": "Tirapat Sukjit",
+        "image": "assets/images/students/tirapat-sukjit.jpg",
+    },
+    {
+        "name": "Tobias Silfverberg",
+        "image": "assets/images/students/tobias-silfverberg.jpg",
+    },
+    {
+        "name": "Wiktoria Dobrzewinska",
+        "image": "assets/images/students/wiktoria-dobrzewinska.jpg",
+    },
 ];
+
+const shuffleArray = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        const temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+};
 
 
 let next = document.querySelector("#next");
@@ -165,7 +174,8 @@ let answersTable = document.querySelector('#answersTable');
 let final = document.querySelector('#final');
 let answers = document.querySelectorAll('#answers > button');
 
-let studentsNew = [...students].sort(() => Math.random() - 0.5);
+let studentsNew = [...students];
+shuffleArray(studentsNew);
 let links = studentsNew.map(student => student.image);
 let names = studentsNew.map(student => student.name);
 let index = 0;
@@ -175,11 +185,13 @@ let userAnswers = [];
 // we need to check if answer is given to avoid score bug
 let answerGiven = false;
 
+
 let newStudent = () => {
     // generate new picture
     studentImg.setAttribute('src', links[index]);
     // shuffle array of names to create random array of names
-    let namesShuffled = [...names].sort(() => Math.random() - 0.5);
+    let namesShuffled = [...names];
+    shuffleArray(namesShuffled);
     // filter our new array to exclude name with the right answer
     namesShuffled = namesShuffled.filter(name => name !== names[index]);
     // add the right name to the begining of array
@@ -193,6 +205,9 @@ let newStudent = () => {
         answers[i].innerText = buttonsAnswers[i];
     }
     console.log('right answer is', names[index])
+    console.log(students);
+    console.log(studentsNew);
+
 }
 
 // create my first page
@@ -265,16 +280,21 @@ next.addEventListener('click', e => {
 
         // highscore logic
         if (highscore) {
-            if (score > highscore) {
+            if (score > highscore && score < names.length) {
                 highscore = score;
-                final.innerHTML += `<p>Good job! Your new highscore is ${highscore}/${names.length}. </p>`;
-            } else if (score = highscore) {
+                final.innerHTML += `<p>Good jobbbb! Your new highscore is ${highscore}/${names.length}. </p>`;
+            } else if (score == names.length) {
+                highscore = score;
                 final.innerHTML +=
                     `<p>You reached the end of game! ${highscore}/${names.length} is maximal points! Go and have a break.</p>`;
             } else {
                 final.innerHTML +=
                     `<p>Sorry, no new highscore. Your previous best result was ${highscore}/${names.length}.</p>`;
             }
+        } else if (score == names.length) {
+            highscore = score;
+            final.innerHTML +=
+                `<p>You reached the end of game! ${highscore}/${names.length} is maximal points! Go and have a break.</p>`;
         } else {
             highscore = score;
             final.innerHTML += `<p>Good job! Your new highscore is ${highscore}/${names.length}. </p>`;
@@ -292,7 +312,7 @@ next.addEventListener('click', e => {
 // starting a new game
 again.addEventListener('click', () => {
 
-    studentsNew = [...students].sort(() => Math.random() - 0.5);
+    shuffleArray(studentsNew);
     links = studentsNew.map(student => student.image);
     names = studentsNew.map(student => student.name);
     index = 0;
